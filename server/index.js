4f5794e0f06b92ac7730dc58864b6b9a5038a6e0
logicscienc <anjuukumari001@gmail.com>
2025-11-05 17:11:13 +0530

    Update cors

--- server/index.js
+++ server/index.js
@@ -23,7 +23,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
 	cors({
-		origin:"https://studynotion-frontend-pi-eight.vercel.app",
+		origin:"https://studynotion-frontend-steel-sigma.vercel.app/",
 		credentials:true,
 	})
 )
